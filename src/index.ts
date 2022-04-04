7265598f48af4fc67fcb676ea99df8fa1b7e83e8
Nathan Rajlich <n@n8.io>
2022-04-03 21:51:04 -0700

    Refactor for `vc build` and caching (#22)
    * Optimize for `vc build`
    * Implement `prepareCache()` function
    * Utilize `IMPORT_TRACE` file for shared import cache

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,56 @@
-import fs from 'fs';
+import fs from 'fs-extra';
+import { tmpdir } from 'os';
 import execa from 'execa';
-import { join } from 'path';
+import fetch from 'node-fetch';
+import { join, dirname, normalize, relative } from 'path';
 import { snakeCase } from 'snake-case';
 import {
-	BuildOptions,
-	Env,
+	FileFsRef,
 	glob,
 	download,
-	createLambda,
-	shouldServe
+	Lambda,
+	shouldServe,
+	BuildV3,
+	PrepareCache,
+	Files,
+	FileBlob,
+	getWriteableDirectory,
+	streamToBuffer,
 } from '@vercel/build-utils';
 
+const TMP = tmpdir();
+
 // `chmod()` is required for usage with `vercel-dev-runtime`
-// since file mode is not preserved in Vercel deployments.
+// since file mode is not preserved in Vercel CLI deployments.
 fs.chmodSync(join(__dirname, 'build.sh'), 0o755);
 fs.chmodSync(join(__dirname, 'bootstrap'), 0o755);
+
+const bootstrapPromise = FileFsRef.fromFsPath({
+	fsPath: join(__dirname, 'bootstrap'),
+});
+const runtimePromise = FileFsRef.fromFsPath({
+	fsPath: join(__dirname, 'runtime.sh'),
+});
+const curlPromise = fetch(
+	'https://github.com/dtschan/curl-static/releases/download/v7.63.0/curl'
+).then(async (res) => {
+	if (!res.ok) {
+		const err = await res.text();
+		throw new Error(`Failed to fetch "curl": ${err}`);
+	}
+	const data = await res.buffer();
+	return new FileBlob({ mode: 0o755, data });
+});
+const jqPromise = fetch(
+	'https://github.com/importpw/static-binaries/raw/master/binaries/linux/x86_64/jq'
+).then(async (res) => {
+	if (!res.ok) {
+		const err = await res.text();
+		throw new Error(`Failed to fetch "jq": ${err}`);
+	}
+	const data = await res.buffer();
+	return new FileBlob({ mode: 0o755, data });
+});
 
 // From this list: https://import.sh/docs/config
 const allowedConfigImports = new Set([
@@ -22,33 +58,32 @@ const allowedConfigImports = new Set([
 	'CURL_OPTS',
 	'DEBUG',
 	'RELOAD',
-	'SERVER'
+	'SERVER',
 ]);
 
 export const version = 3;
 
 export { shouldServe };
 
-export async function build({
+export const build: BuildV3 = async ({
 	workPath,
 	files,
 	entrypoint,
 	meta = {},
-	config = {}
-}: BuildOptions) {
-	const configEnv: Env = {};
-	const { devCacheDir = join(workPath, '.vercel', 'cache') } = meta;
-	const distPath = join(devCacheDir, 'bash', entrypoint);
-
+	config = {},
+}) => {
 	await download(files, workPath, meta);
 
+	const { devCacheDir = join(workPath, '.vercel', 'cache') } = meta;
+	const importCacheDir = join(devCacheDir, 'bash');
+
+	const configEnv: Lambda['environment'] = {};
 	for (const [key, val] of Object.entries(config)) {
 		const name = snakeCase(key).toUpperCase();
 		if (typeof val === 'string' && allowedConfigImports.has(name)) {
 			configEnv[`IMPORT_${name}`] = val;
 		}
 	}
-
 	if (config?.import) {
 		for (const key of Object.keys(config.import)) {
 			const name = snakeCase(key).toUpperCase();
@@ -56,33 +91,114 @@ export async function build({
 		}
 	}
 
-	const IMPORT_CACHE = join(distPath, '.import-cache');
-	const env: Env = {
+	const IMPORT_TRACE = join(TMP, Math.random().toString(16).substring(2));
+
+	const env = {
 		...process.env,
 		...configEnv,
-		PATH: `${IMPORT_CACHE}/bin:${process.env.PATH}`,
-		IMPORT_CACHE,
-		DIST: distPath,
-		BUILDER: __dirname,
+		IMPORT_CACHE: importCacheDir,
+		IMPORT_TRACE,
+		WORK_PATH: workPath,
 		ENTRYPOINT: entrypoint,
-		VERCEL_DEV: meta.isDev ? '1' : '0'
 	};
+
+	const buildDir = await getWriteableDirectory();
 
 	await execa(join(__dirname, 'build.sh'), [], {
 		env,
-		cwd: workPath,
-		stdio: 'inherit'
+		cwd: buildDir,
+		stdio: 'inherit',
 	});
 
-	const output = await createLambda({
-		files: await glob('**', distPath),
-		handler: entrypoint, // not actually used in `bootstrap`
-		runtime: 'provided',
-		environment: {
-			...configEnv,
-			SCRIPT_FILENAME: entrypoint
+	const trace = await fs.readFile(IMPORT_TRACE, 'utf8').then((traceFile) => {
+		const trimmed = traceFile.trim();
+		if (!trimmed) return [];
+		return trimmed.split('\n');
+	});
+	fs.remove(IMPORT_TRACE);
+
+	const lambdaFiles: Files = {
+		...(await filesToBlobs(glob('**', buildDir)).finally(() =>
+			fs.remove(buildDir)
+		)),
+		bootstrap: await bootstrapPromise,
+		'.import-cache/runtime.sh': await runtimePromise,
+		'.import-cache/bin/curl': await curlPromise,
+		'.import-cache/bin/jq': await jqPromise,
+		// For now only the entrypoint file is copied into the lambda
+		[entrypoint]: files[entrypoint],
+	};
+
+	for (const url of trace) {
+		const urlPath = normalize(url.replace('://', '/'));
+		const linkPath = join(importCacheDir, 'links', urlPath);
+		const locationPath = join(importCacheDir, 'locations', urlPath);
+		const [linkFile, locationFile] = await Promise.all([
+			FileFsRef.fromFsPath({ fsPath: linkPath }),
+			FileFsRef.fromFsPath({ fsPath: locationPath }),
+		]);
+		lambdaFiles[join('.import-cache/links', urlPath)] = linkFile;
+		lambdaFiles[join('.import-cache/locations', urlPath)] = locationFile;
+
+		const dataPath = join(dirname(linkPath), await fs.readlink(linkPath));
+		const dataOutputPath = join(
+			'.import-cache',
+			relative(importCacheDir, dataPath)
+		);
+
+		if (!lambdaFiles[dataOutputPath]) {
+			lambdaFiles[dataOutputPath] = await FileFsRef.fromFsPath({
+				fsPath: dataPath,
+			});
 		}
+	}
+
+	// Trace the `bin` dir:
+	//  - if symlink, then include if it points to a traced files
+	//  - if not symlink, then always include in output
+	const binDir = join(importCacheDir, 'bin');
+	const bins = await fs.readdir(binDir);
+	for (const bin of bins) {
+		const binPath = join(binDir, bin);
+		const target = await fs.readlink(binPath).catch((err) => {
+			if (err.code !== "EINVAL") throw err;
+		});
+		if (target) {
+			const rel = relative(importCacheDir, join(binDir, target));
+			if (!lambdaFiles[join(".import-cache", rel)]) {
+				continue;
+			}
+		}
+		lambdaFiles[join(".import-cache/bin", bin)] =
+			await FileFsRef.fromFsPath({
+				fsPath: binPath,
+			});
+	}
+
+	const output = new Lambda({
+		files: lambdaFiles,
+		handler: entrypoint,
+		runtime: 'provided',
+		environment: configEnv,
 	});
 
 	return { output };
+};
+
+export const prepareCache: PrepareCache = async ({ workPath }) => {
+	return await glob('.vercel/cache/bash/**', workPath);
+};
+
+async function filesToBlobs(filesPromise: Promise<Files>) {
+	const files = await filesPromise;
+	for (const [name, file] of Object.entries(files)) {
+		const stream = file.toStream();
+		const buffer = await streamToBuffer(stream);
+		files[name] = new FileBlob({
+			mode: file.mode,
+			contentType: file.contentType,
+			data: buffer,
+		});
+	}
+	return files;
 }
